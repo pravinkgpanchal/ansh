b83bd9f40cb32ec57e6fae1d2153a3a83e1427b2
test test <test@ansh.com>
2018-02-02 15:22:56 +0530

    task 3 comple

--- calculator.php
+++ calculator.php
@@ -17,7 +17,7 @@ class Calculator
         echo $sum = $a+$b;
     }
 
-    
+
     /**
     * Task 2: Capability  to handle multiple numer for addition
     */
@@ -31,6 +31,35 @@ class Calculator
 	    for ($i=0; $i<$num_arg; $i++) 
 	    {
 	    	$addition +=  func_get_arg($i); 
+	    }
+	    echo $addition;
+    }
+
+    /**
+    * Task 3: \n consider as seperator
+    */
+    public function addition()
+    {    
+		// get no. arg pass by user
+	    $num_arg = func_num_args(); 
+
+	    $addition = 0;
+	    for ($i=0; $i<$num_arg; $i++) 
+	    {
+	    	/**
+	    	* Find \n and use as separator
+	    	*/
+	    	$par =  func_get_arg($i);
+	    	if(strpos($par, '\n'))
+	    	{
+	    		$arr = explode('\n',$par);
+	    		$arr_sum = array_sum($arr);
+	    		$addition += $arr_sum;
+	    	}
+	    	else
+	    	{
+	    		$addition +=  func_get_arg($i);
+	    	}	    	
 	    }
 	    echo $addition;
     }
