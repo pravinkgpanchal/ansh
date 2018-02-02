26dd2be39cc07b455543432562c9e81e8c7264bd
test test <test@ansh.com>
2018-02-02 15:16:15 +0530

    first commit

---
+++ calculator.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+* Command Caluculator library for calculation
+*  Name: Pravin Panchal
+*/
+
+
+class Calculator
+{
+	/**
+	* Task 1: Command line utility to sum zero to two number
+	*/
+    public function Sum($a=0, $b=0)
+    {
+        echo $sum = $a+$b;
+    }
+
+}
+
+
+
+
+array_shift($argv);
+$className = array_shift($argv);
+$funcName = array_shift($argv);
+
+call_user_func_array(array($className, $funcName), $argv);
