c5f2be56ed8ff8cc876e11e1fa4fd067725bd451
test test <test@ansh.com>
2018-02-02 15:23:43 +0530

    task 4 Complete

--- calculator.php
+++ calculator.php
@@ -65,6 +65,54 @@ class Calculator
     }
 
 
+     /**
+    * Task 4: find delimiter and usr as seperator
+    */
+    public function addition_delimiter()
+    {    
+		// get no. arg pass by user
+	    $num_arg = func_num_args(); 
+
+	    $addition = 0;
+	    for ($i=0; $i<$num_arg; $i++) 
+	    {
+	    	/**
+	    	* Find delimiter and use as separator
+	    	*/
+	    	$par =  func_get_arg($i);
+	    	
+	    	
+	    	// Find delimiter
+			$f_occ = strpos($par, "\\");
+			$l_occ = strpos($par, "\\", 2);
+			$delimiter = substr($par,$f_occ+2,$l_occ-2);
+
+
+			// slit inot numbers
+			$arg_arr = explode($delimiter, $par);
+
+			// print_r($arg_arr);
+			$second = explode('\\',$arg_arr[1]);
+			$n = count($second);
+			$second = $second[$n-1];
+
+			$addition = $second;
+
+			$remain_arg = count($arg_arr);
+			if($remain_arg>2)
+			{
+
+				for($i=1; $i< $remain_arg ;$i++)
+				{
+					$addition += $arg_arr[$i];
+				}
+			}			
+	    }
+	    echo $addition;
+    }
+
+
+
 }
 
 array_shift($argv);
